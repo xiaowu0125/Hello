697534e6218db6a00dfea0f6e76984c9b0fd3ac3
胡恒洲 <huhengzhou@9jointdemacbook-pro.local>
2016-12-28 10:15:17 +0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by 胡恒洲 on 2016/12/28.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by 胡恒洲 on 2016/12/28.
+//  Copyright © 2016年 FZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
